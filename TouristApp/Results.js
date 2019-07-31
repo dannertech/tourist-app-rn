2430747707163c8f03681e29400f528f6a36cf78
dannertech <danner@danner.tech>
2019-07-30 21:50:21 -0400

    finished Tourist App application

--- TouristApp/Results.js
+++ TouristApp/Results.js
@@ -3,7 +3,32 @@
 import React, {Component} from 'react';
 import {Text, View, TextInput, Button, StyleSheet, FlatList, TouchableHighlight, Image} from 'react-native';
 
+class ListItem extends React.PureComponent {
+    _onPress = () => {
+        this.props.onPressItem(this.props.index);
+    }
 
+    render(){
+        const item = this.props.item;
+        const price = item.price_formatted.split(' ')[0]
+        return(
+            <TouchableHighlight
+            onPress={this._onPress}
+            underlayColor='#dddddd' >
+            <View>
+            <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{ uri: item.img_url }} />
+            <View style={styles.textContainer}>
+            <Text style={styles.price}>{price}</Text>
+            <Text style={styles.title} numberOfLines={1}>{item.title}</Text>
+            </View>
+            </View>
+            <View style={styles.separator} />
+            </View>
+            </TouchableHighlight>
+        )
+    }
+}
 
 export default class Results extends Component {
     constructor(props){
@@ -14,20 +39,18 @@ export default class Results extends Component {
     static navigationOptions = {
         title: 'Results'
     }
+    _onPressItem = (index) => {
+        console.log("Pressed row: "+index);
+    }
     _keyExtractor = (item,index) => index;
 
-    _renderItem = ({item}) => {
-      
-        return(
-            <TouchableHighlight
-                underlayColor='#dddddd'>
-                <View>
-                    <Text>{item.title}</Text>
-                </View>
-                </TouchableHighlight>
-            
-        )
-    }
+    _renderItem = ({item, index}) => (
+        <ListItem
+        item={item}
+        index={index}
+        onPressItem={this._onPressItem}
+        />
+    )
     render(){
         return(
             <FlatList
@@ -38,3 +61,31 @@ export default class Results extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    thumb: {
+        width: 80, 
+        height: 80,
+        marginRight: 10,
+    },
+    textContainer: {
+        flex: 1
+    },
+    separator: {
+        height: 1,
+        backgroundColor: '#dddddd'
+    },
+    price: {
+        fontSize: 20,
+        color: '#48BBEC',
+        fontWeight: 'bold'
+    },
+    title: {
+        fontSize: 20,
+        color: '#656565'
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        padding: 10
+    }
+})
